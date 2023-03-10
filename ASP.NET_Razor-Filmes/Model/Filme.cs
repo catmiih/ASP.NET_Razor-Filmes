8c3807c0426d54210b4807e3cee91aab20dc2f76
catmiih <emilyleme.dev@gmail.com>
2023-03-10 11:26:27 -0300

    feat: Update front-end to Portuguese Language

--- ASP.NET_Razor-Filmes/Model/Filme.cs
+++ ASP.NET_Razor-Filmes/Model/Filme.cs
@@ -7,8 +7,13 @@ namespace RazorPagesFilmes.Model
         public int ID { get; set; }
         public string Titulo { get; set; } = string.Empty;
 
+        [Display(Name = "Data Lançamento")]
         [DataType(DataType.Date)] public DateTime DataLancamento { get; set; }
+
+        [Display(Name = "Gênero")]
         public string Genero { get; set; } = string.Empty;
+
+        [Display(Name = "Preço")]
         public decimal Preco { get; set; }
 
 
